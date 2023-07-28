7faa83e0d572e381d2766f0d44099ccba118bc65
deba793 <debasisrout793@gmail.com>
2023-07-28 15:47:29 +0530

    Assignment-18

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using assignment_18;
+using System;
+namespace assignment_18
+{
+
+
+    class  Program
+    {
+        static void Main()
+        {
+            Smartphone smartphone = new Smartphone();
+            smartphone.Connect();
+            smartphone.Charge(98);
+            Console.WriteLine($"Smartphone connected: {smartphone.Connect()}");
+            Console.WriteLine($"Smartphone Battery Level: {smartphone.BatteryLevel}");
+            Console.WriteLine(smartphone.Display());
+
+            Console.WriteLine();
+
+            Laptop laptop = new Laptop();
+            laptop.Connect();
+            laptop.Charge(79);
+            Console.WriteLine($"Laptop connected: {laptop.Connect()}");
+            Console.WriteLine($"Laptop Battery Level: {laptop.BatteryLevel}");
+            Console.WriteLine(laptop.Display());
+        }
+    }
+}
---
+++ Smartphone.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_18
+{
+    public class Smartphone : IConnectable, IRechargeable, IDisplayable
+    {
+        private bool isConnected;
+        private int batteryLevel;
+        private string displayInfo;
+
+        public Smartphone()
+        {
+
+            isConnected = false;
+            batteryLevel = 0;
+            displayInfo = "Smartphone display information...It's working well";
+        }
+
+        public bool Connect()
+        {
+            isConnected = true;
+            return isConnected;
+        }
+        public void Charge(int minutes)
+        {
+            batteryLevel += minutes;
+        }
+
+        public string Display()
+        {
+            return displayInfo;
+        }
+
+        public int BatteryLevel
+        {
+            get { return batteryLevel; }
+        }
+    }
+}
